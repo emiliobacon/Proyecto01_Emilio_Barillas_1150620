3567fbf70a7c87a9ee17bdc6cb1fa156b68fc606
EMILIO ANTONIO BARILLAS CONTRERAS <eabarillasco@correo.url.edu.gt>
2021-10-24 06:57:48 -0600

    push

--- MyForm.h
+++ MyForm.h
@@ -154,19 +154,21 @@ namespace PartyMix {
 			// lblPlaylist
 			// 
 			this->lblPlaylist->AutoSize = true;
+			this->lblPlaylist->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 8.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
 			this->lblPlaylist->ForeColor = System::Drawing::Color::White;
 			this->lblPlaylist->Location = System::Drawing::Point(12, 120);
 			this->lblPlaylist->Name = L"lblPlaylist";
-			this->lblPlaylist->Size = System::Drawing::Size(39, 13);
+			this->lblPlaylist->Size = System::Drawing::Size(47, 13);
 			this->lblPlaylist->TabIndex = 2;
 			this->lblPlaylist->Text = L"Playlist";
 			// 
 			// btnReproducir
 			// 
 			this->btnReproducir->BackColor = System::Drawing::Color::LimeGreen;
-			this->btnReproducir->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 8.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+			this->btnReproducir->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 8.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->btnReproducir->ForeColor = System::Drawing::SystemColors::ButtonFace;
+			this->btnReproducir->ForeColor = System::Drawing::Color::Black;
 			this->btnReproducir->Location = System::Drawing::Point(135, 51);
 			this->btnReproducir->Name = L"btnReproducir";
 			this->btnReproducir->Size = System::Drawing::Size(105, 33);
@@ -178,21 +180,27 @@ namespace PartyMix {
 			// lblTamañoPlaylist
 			// 
 			this->lblTamañoPlaylist->AutoSize = true;
+			this->lblTamañoPlaylist->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 8.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
 			this->lblTamañoPlaylist->ForeColor = System::Drawing::Color::White;
 			this->lblTamañoPlaylist->Location = System::Drawing::Point(12, 136);
 			this->lblTamañoPlaylist->Name = L"lblTamañoPlaylist";
-			this->lblTamañoPlaylist->Size = System::Drawing::Size(66, 13);
+			this->lblTamañoPlaylist->Size = System::Drawing::Size(78, 13);
 			this->lblTamañoPlaylist->TabIndex = 6;
 			this->lblTamañoPlaylist->Text = L"Canciones:  ";
 			// 
 			// btnBorrarPlaylist
 			// 
+			this->btnBorrarPlaylist->BackColor = System::Drawing::Color::LimeGreen;
+			this->btnBorrarPlaylist->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 8.25F, System::Drawing::FontStyle::Regular,
+				System::Drawing::GraphicsUnit::Point, static_cast<System::Byte>(0)));
+			this->btnBorrarPlaylist->ForeColor = System::Drawing::Color::Black;
 			this->btnBorrarPlaylist->Location = System::Drawing::Point(246, 51);
 			this->btnBorrarPlaylist->Name = L"btnBorrarPlaylist";
 			this->btnBorrarPlaylist->Size = System::Drawing::Size(105, 33);
 			this->btnBorrarPlaylist->TabIndex = 7;
 			this->btnBorrarPlaylist->Text = L"Borrar Playlist";
-			this->btnBorrarPlaylist->UseVisualStyleBackColor = true;
+			this->btnBorrarPlaylist->UseVisualStyleBackColor = false;
 			this->btnBorrarPlaylist->Click += gcnew System::EventHandler(this, &MyForm::btnBorrarPlaylist_Click);
 			// 
 			// lblMostrarPlaylist
@@ -223,48 +231,62 @@ namespace PartyMix {
 			// lblTitulo
 			// 
 			this->lblTitulo->AutoSize = true;
+			this->lblTitulo->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 8.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->lblTitulo->ForeColor = System::Drawing::Color::White;
 			this->lblTitulo->Location = System::Drawing::Point(535, 35);
 			this->lblTitulo->Name = L"lblTitulo";
-			this->lblTitulo->Size = System::Drawing::Size(35, 13);
+			this->lblTitulo->Size = System::Drawing::Size(41, 13);
 			this->lblTitulo->TabIndex = 12;
 			this->lblTitulo->Text = L"Título";
 			// 
 			// lblArtista
 			// 
 			this->lblArtista->AutoSize = true;
+			this->lblArtista->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 8.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->lblArtista->ForeColor = System::Drawing::Color::White;
 			this->lblArtista->Location = System::Drawing::Point(653, 35);
 			this->lblArtista->Name = L"lblArtista";
-			this->lblArtista->Size = System::Drawing::Size(36, 13);
+			this->lblArtista->Size = System::Drawing::Size(43, 13);
 			this->lblArtista->TabIndex = 13;
 			this->lblArtista->Text = L"Artista";
 			// 
 			// lblBuscarCanciones
 			// 
 			this->lblBuscarCanciones->AutoSize = true;
+			this->lblBuscarCanciones->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 8.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->lblBuscarCanciones->ForeColor = System::Drawing::Color::White;
 			this->lblBuscarCanciones->Location = System::Drawing::Point(535, 13);
 			this->lblBuscarCanciones->Name = L"lblBuscarCanciones";
-			this->lblBuscarCanciones->Size = System::Drawing::Size(93, 13);
+			this->lblBuscarCanciones->Size = System::Drawing::Size(109, 13);
 			this->lblBuscarCanciones->TabIndex = 14;
 			this->lblBuscarCanciones->Text = L"Buscar Canciones";
 			// 
 			// btnExportarPlaylist
 			// 
+			this->btnExportarPlaylist->BackColor = System::Drawing::Color::LimeGreen;
+			this->btnExportarPlaylist->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 8.25F, System::Drawing::FontStyle::Regular,
+				System::Drawing::GraphicsUnit::Point, static_cast<System::Byte>(0)));
+			this->btnExportarPlaylist->ForeColor = System::Drawing::Color::Black;
 			this->btnExportarPlaylist->Location = System::Drawing::Point(357, 51);
 			this->btnExportarPlaylist->Name = L"btnExportarPlaylist";
-			this->btnExportarPlaylist->Size = System::Drawing::Size(105, 33);
+			this->btnExportarPlaylist->Size = System::Drawing::Size(111, 33);
 			this->btnExportarPlaylist->TabIndex = 9;
 			this->btnExportarPlaylist->Text = L"Exportar Playlist";
-			this->btnExportarPlaylist->UseVisualStyleBackColor = true;
+			this->btnExportarPlaylist->UseVisualStyleBackColor = false;
 			this->btnExportarPlaylist->Click += gcnew System::EventHandler(this, &MyForm::btnExportarPlaylist_Click);
 			// 
 			// btnAgregaraCola
 			// 
+			this->btnAgregaraCola->BackColor = System::Drawing::Color::LimeGreen;
 			this->btnAgregaraCola->Location = System::Drawing::Point(538, 77);
 			this->btnAgregaraCola->Name = L"btnAgregaraCola";
 			this->btnAgregaraCola->Size = System::Drawing::Size(105, 33);
 			this->btnAgregaraCola->TabIndex = 15;
 			this->btnAgregaraCola->Text = L"Agregar a la Cola";
-			this->btnAgregaraCola->UseVisualStyleBackColor = true;
+			this->btnAgregaraCola->UseVisualStyleBackColor = false;
 			this->btnAgregaraCola->Click += gcnew System::EventHandler(this, &MyForm::btnAgregaraCola_Click);
 			// 
 			// lblMostrarCola
@@ -272,6 +294,7 @@ namespace PartyMix {
 			this->lblMostrarCola->AutoSize = true;
 			this->lblMostrarCola->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 8.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
+			this->lblMostrarCola->ForeColor = System::Drawing::Color::White;
 			this->lblMostrarCola->Location = System::Drawing::Point(535, 204);
 			this->lblMostrarCola->Name = L"lblMostrarCola";
 			this->lblMostrarCola->Size = System::Drawing::Size(0, 13);
@@ -280,59 +303,73 @@ namespace PartyMix {
 			// lblCola
 			// 
 			this->lblCola->AutoSize = true;
+			this->lblCola->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 8.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->lblCola->ForeColor = System::Drawing::Color::White;
 			this->lblCola->Location = System::Drawing::Point(535, 161);
 			this->lblCola->Name = L"lblCola";
-			this->lblCola->Size = System::Drawing::Size(28, 13);
+			this->lblCola->Size = System::Drawing::Size(32, 13);
 			this->lblCola->TabIndex = 17;
 			this->lblCola->Text = L"Cola";
 			// 
 			// lblCancionesCola
 			// 
 			this->lblCancionesCola->AutoSize = true;
+			this->lblCancionesCola->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 8.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->lblCancionesCola->ForeColor = System::Drawing::Color::White;
 			this->lblCancionesCola->Location = System::Drawing::Point(535, 177);
 			this->lblCancionesCola->Name = L"lblCancionesCola";
-			this->lblCancionesCola->Size = System::Drawing::Size(63, 13);
+			this->lblCancionesCola->Size = System::Drawing::Size(74, 13);
 			this->lblCancionesCola->TabIndex = 18;
 			this->lblCancionesCola->Text = L"Canciones: ";
 			// 
 			// btnBorrarCola
 			// 
+			this->btnBorrarCola->BackColor = System::Drawing::Color::LimeGreen;
 			this->btnBorrarCola->Location = System::Drawing::Point(656, 116);
 			this->btnBorrarCola->Name = L"btnBorrarCola";
 			this->btnBorrarCola->Size = System::Drawing::Size(105, 33);
 			this->btnBorrarCola->TabIndex = 19;
 			this->btnBorrarCola->Text = L"Borrar Cola";
-			this->btnBorrarCola->UseVisualStyleBackColor = true;
+			this->btnBorrarCola->UseVisualStyleBackColor = false;
 			this->btnBorrarCola->Click += gcnew System::EventHandler(this, &MyForm::btnBorrarCola_Click);
 			// 
 			// btnDesencolar
 			// 
+			this->btnDesencolar->BackColor = System::Drawing::Color::LimeGreen;
 			this->btnDesencolar->Location = System::Drawing::Point(538, 116);
 			this->btnDesencolar->Name = L"btnDesencolar";
 			this->btnDesencolar->Size = System::Drawing::Size(105, 33);
 			this->btnDesencolar->TabIndex = 20;
 			this->btnDesencolar->Text = L"Eliminar de la Cola";
-			this->btnDesencolar->UseVisualStyleBackColor = true;
+			this->btnDesencolar->UseVisualStyleBackColor = false;
 			this->btnDesencolar->Click += gcnew System::EventHandler(this, &MyForm::btnDesencolar_Click);
 			// 
 			// btnReproducirAnterior
 			// 
+			this->btnReproducirAnterior->BackColor = System::Drawing::Color::LimeGreen;
+			this->btnReproducirAnterior->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 8.25F, System::Drawing::FontStyle::Regular,
+				System::Drawing::GraphicsUnit::Point, static_cast<System::Byte>(0)));
 			this->btnReproducirAnterior->Location = System::Drawing::Point(538, 312);
 			this->btnReproducirAnterior->Name = L"btnReproducirAnterior";
 			this->btnReproducirAnterior->Size = System::Drawing::Size(105, 47);
 			this->btnReproducirAnterior->TabIndex = 21;
 			this->btnReproducirAnterior->Text = L"Reproducir Anterior";
-			this->btnReproducirAnterior->UseVisualStyleBackColor = true;
+			this->btnReproducirAnterior->UseVisualStyleBackColor = false;
 			this->btnReproducirAnterior->Click += gcnew System::EventHandler(this, &MyForm::btnReproducirAnterior_Click);
 			// 
 			// btnReproducirSiguiente
 			// 
+			this->btnReproducirSiguiente->BackColor = System::Drawing::Color::LimeGreen;
+			this->btnReproducirSiguiente->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 8.25F, System::Drawing::FontStyle::Regular,
+				System::Drawing::GraphicsUnit::Point, static_cast<System::Byte>(0)));
 			this->btnReproducirSiguiente->Location = System::Drawing::Point(649, 312);
 			this->btnReproducirSiguiente->Name = L"btnReproducirSiguiente";
 			this->btnReproducirSiguiente->Size = System::Drawing::Size(105, 47);
 			this->btnReproducirSiguiente->TabIndex = 22;
 			this->btnReproducirSiguiente->Text = L"Reproducir Siguiente";
-			this->btnReproducirSiguiente->UseVisualStyleBackColor = true;
+			this->btnReproducirSiguiente->UseVisualStyleBackColor = false;
 			this->btnReproducirSiguiente->Click += gcnew System::EventHandler(this, &MyForm::btnReproducirSiguiente_Click);
 			// 
 			// label1
@@ -360,12 +397,15 @@ namespace PartyMix {
 			// 
 			// btnSync
 			// 
+			this->btnSync->BackColor = System::Drawing::Color::LimeGreen;
+			this->btnSync->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 8.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
 			this->btnSync->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"btnSync.Image")));
 			this->btnSync->Location = System::Drawing::Point(463, 303);
 			this->btnSync->Name = L"btnSync";
 			this->btnSync->Size = System::Drawing::Size(58, 56);
 			this->btnSync->TabIndex = 25;
-			this->btnSync->UseVisualStyleBackColor = true;
+			this->btnSync->UseVisualStyleBackColor = false;
 			this->btnSync->Click += gcnew System::EventHandler(this, &MyForm::btnSync_Click);
 			// 
 			// MyForm
